1995c7c5da8c406c49ee336a0583a73fb9484858
Tunuguntla Mohana Sai <mohana.sai1204@gmail.com>
2026-02-18 15:52:32 +0530

    Initital Maven project structure

---
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -0,0 +1,12 @@
+package com.quantitymeasurement;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuantityTest {
+
+    @Test
+    void shouldPassBasicTest() {
+        assertEquals(5, 2 + 3);
+    }
+}
