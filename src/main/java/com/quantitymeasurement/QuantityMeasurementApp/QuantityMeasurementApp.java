5691da6d5346cf4b3a9db78c8dd672faf6b10b35
Tunuguntla Mohana Sai <mohana.sai1204@gmail.com>
2026-02-24 10:42:08 +0530

    [Tunuguntla Mohana Sai] refactored the UC1 code

--- src/main/java/com/quantitymeasurement/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -1,49 +1,36 @@
 package com.quantitymeasurement.QuantityMeasurementApp;
 
-import java.util.Objects;
-
-/**
- * Hello world!
- *
- */
 public class QuantityMeasurementApp {
-
 	public static class Feet {
 		private final double value;
 
-		// Constructor
 		public Feet(double value) {
 			this.value = value;
 		}
 
-		public double getValue() {
-			return value;
-		}
-
-		@Override
-		public int hashCode() {
-			return Double.hashCode(value);
-		}
-
 		@Override
 		public boolean equals(Object obj) {
+			// Check if both references point to the same object
 			if (this == obj)
 				return true;
-			if (obj == null)
+
+			// Check if the object is null or of different class
+			if (obj == null || getClass() != obj.getClass())
 				return false;
-			if (getClass() != obj.getClass())
-				return false;
+			;
+
+			// Safe casting
 			Feet other = (Feet) obj;
+
 			return Double.compare(this.value, other.value) == 0;
 		}
 
 	}
 
 	public static void main(String[] args) {
+		Feet feet1 = new Feet(1.0);
+		Feet feet2 = new Feet(1.0);
 
-		Feet value1 = new Feet(1.0);
-		Feet value2 = new Feet(1.0);
-
-		System.out.println(value1.equals(value2)); // true
+		System.out.println(feet1.equals(feet2));
 	}
 }
--- src/test/java/com/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -1,51 +1,49 @@
 package com.quantitymeasurement;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 import com.quantitymeasurement.QuantityMeasurementApp.QuantityMeasurementApp;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class QuantityMeasurementAppTest {
+public class QuantityMeasurementAppTest {
 
 	@Test
-	void testFeetEquality_SameValue() {
-		QuantityMeasurementApp.Feet value1 = new QuantityMeasurementApp.Feet(1.0);
-		QuantityMeasurementApp.Feet value2 = new QuantityMeasurementApp.Feet(1.0);
+	public void testEquality_SameValue() {
+		QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
+		QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(1.0);
 
-		assertTrue(value1.equals(value2), "Feet objects with the same value should be equal");
+		assertTrue(feet1.equals(feet2));
 	}
 
 	@Test
-	void testFeetEquality_DifferentValue() {
-		QuantityMeasurementApp.Feet value1 = new QuantityMeasurementApp.Feet(1.0);
-		QuantityMeasurementApp.Feet value2 = new QuantityMeasurementApp.Feet(2.0);
+	public void testEquality_DifferentValue() {
+		QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
+		QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(2.0);
 
-		assertFalse(value1.equals(value2), "Feet objects with the different value should not be equal");
-
+		assertFalse(feet1.equals(feet2));
 	}
 
 	@Test
-	void testFeetEquality_NullComparision() {
-		QuantityMeasurementApp.Feet value1 = new QuantityMeasurementApp.Feet(2.0);
+	public void testEquality_NullComparison() {
+		QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
 
-		assertFalse(value1.equals(null), "Feet object should not be equal to null");
+		assertFalse(feet1.equals(null));
 	}
 
 	@Test
-	void testFeetEquality_DifferentClass() {
-		QuantityMeasurementApp.Feet value1 = new QuantityMeasurementApp.Feet(2.0);
-
-		String nonFeet = "non feet";
-
-		assertFalse(value1.equals(nonFeet), "Feet object should not be equal to an object of different class");
+	public void testEquality_NonNumericInput() {
+		QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
+		char nonNumericInput = 'a';
+		assertFalse(feet1.equals(nonNumericInput));
 	}
 
 	@Test
-	void testFeetEquality_SameReference() {
-		QuantityMeasurementApp.Feet value1 = new QuantityMeasurementApp.Feet(2.0);
+	public void testEquality_SameReference() {
+		QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
+		QuantityMeasurementApp.Feet feet2 = feet1; // Same reference
 
-		assertTrue(value1.equals(value1), "Feet object should be equal to itself");
-
+		assertTrue(feet1.equals(feet2));
 	}
+
 }
